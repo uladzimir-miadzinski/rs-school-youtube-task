172700f682d44eb59540e0822716f92360d9b4f2
medinsky <webmedinsky@gmail.com>
2019-05-06 01:36:13 +0300

    move app.bundle to js dir

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,6 @@ module.exports = {
     entry: './src/app.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'app.bundle.js'
+        filename: 'js/app.bundle.js'
     }
 };
